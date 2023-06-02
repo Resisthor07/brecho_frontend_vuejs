d773f227afcaec6408d9a7ab8786cab5ef0b42a5
Thiago Cavalcante <116036410+Thiago-cavalcante-oliveira@users.noreply.github.com>
2023-06-02 16:36:44 -0300

    header

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+   <header-componente></header-componente>
+  </div>
+</template>
+
+<script>
+
+import HeaderComponente from "@/components/header-componente";
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComponente,
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/header-componente.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container ">
+    <div class="level">
+      <div class="columns  is-vcentered is-centered">
+        <div class="column is-3 level-left has-text-left">
+          <b-image
+              class="is-size-1"
+              alt="Vue logo"
+              :src="require('@/assets/logo.png')"></b-image>
+        </div>
+      </div>
+      <div class="column is-6 ">
+        <b-field>
+          <b-input placeholder="Busque aqui..." rounded></b-input>
+        </b-field>
+      </div>
+      <div class="column is-3">
+        <svg-icon class="mr-4" type="mdi" :path="mdiShoppingOutline"></svg-icon>
+        <svg-icon cla type="mdi" :path="mdiStarOutline"></svg-icon>
+      </div>
+
+
+
+    </div>
+    <div class="notification is-primary">
+      teste
+    </div>
+  </section>
+</template>
+
+<script>
+import SvgIcon from '@jamescoyle/vue-icon';
+import {mdiShoppingOutline} from '@mdi/js';
+import {mdiStarOutline} from '@mdi/js';
+
+export default {
+  name: "header-componente",
+  components: {
+    SvgIcon
+  },
+  data() {
+    return {
+      mdiShoppingOutline, mdiStarOutline
+
+    }
+  }
+}
+
+
+</script>
+
+<style scoped>
+
+
+</style>
