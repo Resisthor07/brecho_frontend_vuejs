76cf8f5a96de627456de59af7dd4b268b7965e1a
Thiago Cavalcante <116036410+Thiago-cavalcante-oliveira@users.noreply.github.com>
2023-06-05 17:37:17 -0300

    construindo o header-componente.vue

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#FF7272', // #E53935
+                secondary: 'rgba(255,114,114,0.7)', // #FFCDD2
+
+            },
+        },
+    },
+})
