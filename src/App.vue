11bf4a0e7689683c045b7b745d288c2be40e90f3
Administrador <thhyagofoz@gmail.com>
2023-06-05 18:24:45 -0300

    Ajustando o header

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <v-app>
-
-
-
-
-
     <v-main>
       <header-componente></header-componente>
     </v-main>
-
   </v-app>
 </template>
 
 <script>
 
 import HeaderComponente from "@/components/header-componente";
-import {mdiShoppingOutline} from '@mdi/js';
-
-
 export default {
   name: 'App',
-
   components: {
-
     HeaderComponente,
-
   },
-
   methods:{
-
     click : function (){
       console.log("teste")
     }
@@ -38,15 +24,11 @@ export default {
 
 
   data: () => ({
-    mdiShoppingOutline,
-    contaitens: 0,
+
+
 
 
   }),
 };
 </script>
-<style>
-.borda{
-  border-color: #FF7272;
-}
-</style>
+
--- src/components/header-componente.vue
+++ src/components/header-componente.vue
@@ -7,14 +7,12 @@
         color="null"
     >
       <v-row class="d-flex justify-space-between">
-        <v-col  class="text-left">
+        <v-col class="text-left">
           <v-img
               @click="click()"
               height="60"
               position=" left center"
               contain
-              alt="Vuetify Logo"
-
 
               src="../assets/logo.png"
               transition="scale-transition"
@@ -23,75 +21,49 @@
         <v-col>
           <v-text-field class="borda"
                         outlined
-
-
-
                         label="Busque aqui..."
                         color="primary"
                         rounded
                         dense
           ></v-text-field>
-
         </v-col>
-
-
         <v-col align="right">
           <v-btn elevation="0" color="white">
 
             <v-icon>mdi-shopping-outline</v-icon>
             <span class="text--accent-2 ">{{ contaitens }} Itens</span>
           </v-btn>
-          <v-btn  elevation="0" color="white" class="ml-2 ba">
+          <v-btn elevation="0" color="white" class="ml-2 ba">
             <v-icon>mdi-star-outline</v-icon>
             <span class="text--darken-3 ">Favoritos</span>
-
           </v-btn>
         </v-col>
       </v-row>
     </v-container>
-
-    <v-main>
-      <header-componente></header-componente>
-    </v-main>
 
   </v-app>
 </template>
 
 <script>
 
-import HeaderComponente from "@/components/header-componente";
-import {mdiShoppingOutline} from '@mdi/js';
-
 
 export default {
   name: 'App',
-
-  components: {
-
-    HeaderComponente,
-
-  },
-
-  methods:{
-
-    click : function (){
+  components: {},
+  methods: {
+    click: function () {
       console.log("teste")
     }
   },
 
-
-
-
   data: () => ({
-    mdiShoppingOutline,
     contaitens: 0,
-
 
   }),
 };
 </script>
 <style>
-.borda{
+.borda {
   border-color: #FF7272;
 }
 </style>
