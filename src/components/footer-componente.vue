be8e1c6f28898a81f8344a53fe21fedc7e5c5c55
Rafael Ch <rafael.cherman0@gmail.com>
2023-06-07 16:17:17 -0300

    ajustado posiçoes do footer, adicionado parte de cima e de baixo do footer

--- src/components/footer-componente.vue
+++ src/components/footer-componente.vue
@@ -3,59 +3,66 @@
         <v-footer
         color="primary"
         absolute="true"
+        padless
         >
 
-            <v-spacer></v-spacer>
-        
-            <v-list color="primary">
-                <v-list-item>
-                    <v-list-item-title class="titulo">Instrucional</v-list-item-title>
-                </v-list-item>
+            <div class="parte_de_cima">
+                <v-spacer></v-spacer>
+            
+                <v-list color="primary">
+                    <v-list-item>
+                        <v-list-item-title class="titulo">Instrucional</v-list-item-title>
+                    </v-list-item>
 
-                <v-list-item
-                v-for="item in instrucional"
-                :key="item">
-                    <v-list-item-title v-text="item" class="lista"></v-list-item-title>
-                </v-list-item>
-            </v-list>
+                    <v-list-item
+                    v-for="item in instrucional"
+                    :key="item">
+                        <v-list-item-title v-text="item" class="lista"></v-list-item-title>
+                    </v-list-item>
+                </v-list>
 
-            <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-            <v-list color="primary">
-                <v-list-item>
-                    <v-list-item-title class="titulo">Pagamentos</v-list-item-title>
-                </v-list-item>
-            </v-list>
+                <v-list color="primary">
+                    <v-list-item>
+                        <v-list-item-title class="titulo">Pagamentos</v-list-item-title>
+                    </v-list-item>
+                </v-list>
 
-            <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-            <v-list color="primary">
-                <v-list-item>
-                    <v-list-item-title class="titulo">Contato</v-list-item-title>
-                </v-list-item>
+                <v-list color="primary">
+                    <v-list-item>
+                        <v-list-item-title class="titulo">Contato</v-list-item-title>
+                    </v-list-item>
 
-                <v-list-item
-                v-for="item in contato"
-                :key="item">
-                    <v-list-item-title v-text="item.text" class="lista"></v-list-item-title>
-                </v-list-item>
-            </v-list>
+                    <v-list-item
+                    v-for="item in contato"
+                    :key="item">
+                        <v-list-item-title v-text="item.text" class="lista"></v-list-item-title>
+                    </v-list-item>
+                </v-list>
 
-            <v-spacer></v-spacer>
+                <v-spacer></v-spacer>
 
-            <v-list color="primary">
-                <v-list-item>
-                    <v-list-item-title class="titulo">Redes Sociais</v-list-item-title>
-                </v-list-item>
+                <v-list color="primary">
+                    <v-list-item>
+                        <v-list-item-title class="titulo">Redes Sociais</v-list-item-title>
+                    </v-list-item>
 
-                <v-list-item
-                v-for="item in redes"
-                :key="item">
-                    <v-list-item-title v-text="item.text" class="lista"></v-list-item-title>
-                </v-list-item>
-            </v-list>
-        
-            <v-spacer></v-spacer>
+                    <v-list-item
+                    v-for="item in redes"
+                    :key="item">
+                        <v-list-item-title v-text="item.text" class="lista"></v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            
+                <v-spacer></v-spacer>
+            </div>
+            <div class="parte_de_baixo">
+                <p class="lista paragrafo">2023 todos os direitos reservados - Desenvolvido por G7</p>
+            </div>
+
 
         </v-footer>
     </v-app>
@@ -98,14 +105,46 @@
 
 <style>
 
+    @import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap');
+
     .titulo
     {
         color: #FFFFFF;
+        font-family: "Dongle";
+        font-style: normal;
+        font-size: 30px;
+        font-weight: 400;
     }
 
     .lista
     {
         color: #FFFFFF;
+        font-family: "Dongle";
+        font-style: normal;
+        font-size: 30px;
+        font-weight: 300;
+    }
+
+    .parte_de_cima
+    {
+        margin-top: 30px;
+        margin-bottom: 30px;
+        display: flex;
+        width: 100%;
+    }
+
+    .parte_de_baixo
+    {   
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-top: 4px solid #FFFFFF ;
+    }
+
+    .paragrafo
+    {
+        margin: 15px;
     }
 
     
