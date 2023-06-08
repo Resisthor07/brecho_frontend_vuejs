66e83e4171fb5a2ce030fe53c617ba022577016e
Rafael Ch <rafael.cherman0@gmail.com>
2023-06-08 20:13:58 -0300

    feito função para adicionar objetos ao array produtos ao carregar a pagina

--- src/components/listadedesejos-view.vue
+++ src/components/listadedesejos-view.vue
@@ -2,7 +2,8 @@
     <v-app>
         <v-main>
             <header-componente></header-componente>
-            <product-card :produto="produtos"></product-card>
+            <scroll-home v-if="!vazio" :produtos="produtos"></scroll-home>
+            <h1 v-if="vazio">Sua lista de desejos está vazia</h1>
             <footer-componente></footer-componente>
         </v-main>
     </v-app>
@@ -13,25 +14,46 @@
 
 import HeaderComponente from "@/components/header-componente";
 import FooterComponente from "@/components/footer-componente";
-import ProductCard from "@/components/productCard";
+import ScrollHome from "@/components/ScrollHome";
+
 
     export default{
         name: "listaDeDesejosView",
         components: {
             HeaderComponente,
-            ProductCard,
+            ScrollHome,
             FooterComponente
         },
         data: () => ({
-            produtos:[
-            {
-                nome: "Calça",
-                valorAtual: "100,23",
-                imagem: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
-                favorite: true
+            produtos:[],
+            vazio: true,
+            quantidade: 0
+        }),
+        methods: {
+            adicionaProdutos(){
+                for(let i=0;i<0;i++)
+                {
+                    this.produtos.push({
+                        nome: "Calça",
+                        valorAtual: "100,23",
+                        imagem: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
+                        favorite: true
+                    })
+                }
+                if(this.produtos.length == 0)
+                {
+                    this.vazio = true;
+                }
+                else
+                {
+                    this.vazio = false;
+                    this.quantidade = this.produtos.length;
+                }
             }
-        ]
-        })
+        },
+        created: function(){
+            this.adicionaProdutos();
+        }
     }
 </script>
 
