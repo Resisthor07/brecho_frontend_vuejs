7e5354a33a687d20ebfe30b7ceaa6ecb1c76126b
Rafael Ch <rafael.cherman0@gmail.com>
2023-06-08 18:39:25 -0300

    adicionado componentes

---
+++ src/components/listadedesejos-view.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-app>
+        <v-main>
+            <header-componente></header-componente>
+            <product-card :produto="produtos"></product-card>
+            <footer-componente></footer-componente>
+        </v-main>
+    </v-app>
+    
+</template>
+
+<script>
+
+import HeaderComponente from "@/components/header-componente";
+import FooterComponente from "@/components/footer-componente";
+import ProductCard from "@/components/productCard";
+
+    export default{
+        name: "listaDeDesejosView",
+        components: {
+            HeaderComponente,
+            ProductCard,
+            FooterComponente
+        },
+        data: () => ({
+            produtos:[
+            {
+                nome: "Calça",
+                valorAtual: "100,23",
+                imagem: "https://cdn.vuetifyjs.com/images/cards/cooking.png",
+                favorite: true
+            }
+        ]
+        })
+    }
+</script>
+
+<style>
+
+</style>
