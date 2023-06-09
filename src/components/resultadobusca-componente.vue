a97d385e792a87752552f238e80d2071ac863ead
Rafael Ch <rafael.cherman0@gmail.com>
2023-06-09 17:25:37 -0300

    componente finalizado

--- src/components/resultadobusca-componente.vue
+++ src/components/resultadobusca-componente.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-
-    </div>
+    <v-main>
+        <p class="ajusta_buscac">RESULTADO DA BUSCA: "{{ consulta }}"</p>
+        <section class="organiza_buscac">
+            <div
+            v-for="i in produtos"
+            :key="i">
+                <product-card :produto="i"></product-card>
+            </div>
+        </section>
+    </v-main>
 </template>
   
 <script>
@@ -23,8 +30,28 @@
   
   
   }
-  </script>
+</script>
   
-  <style scoped>
-  
-  </style>
+<style scoped>
+
+    @import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap');
+
+
+    .organiza_buscac
+    {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .ajusta_buscac
+    {
+        margin-left: 60px;
+        font-family: "Dongle";
+        font-size: 30px;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+</style>
