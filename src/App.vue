d57582703be31a37b697a034e6367bb587cb3735
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-06-11 15:30:00 -0300

    Padronizando tamanho de fontes

--- src/App.vue
+++ src/App.vue
@@ -57,6 +57,7 @@ export default {
   }),
 };
 </script>
+
 <style>
   @font-face {
     font-family: 'Poppins Bold';
@@ -92,7 +93,53 @@ export default {
     font-weight: normal;
     font-style: normal;
   }
-  
+
+  .poppins-bold{
+    font-family: 'Poppins Bold';
+  }
+  .poppins-light{
+    font-family: 'Poppins Light';
+  }
+  .poppins-medium{
+    font-family: 'Poppins Medium';
+  }
+  .poppins-regular{
+    font-family: 'Poppins Regular';
+  }
+  .poppins-semibold{
+    font-family: 'Poppins SemiBold';
+  }
+
+  .f12{font-size: 0.75rem;}
+  .f13{font-size: 0.813rem;}
+  .f14{font-size: 0.875rem;}
+  .f15{font-size: 0.938rem;}
+  .f16{font-size: 1rem;}
+  .f17{font-size: 1.063rem;}
+  .f18{font-size: 1.125rem;}
+  .f19{font-size: 1.188rem;}
+  .f20{font-size: 1.25rem;}
+  .f21{font-size: 1.313rem;}
+  .f22{font-size: 1.375rem;}
+  .f23{font-size: 1.438rem;}
+  .f24{font-size: 1.5rem;}
+  .f25{font-size: 1.563rem;}
+  .f26{font-size: 1.625rem;}
+  .f27{font-size: 1.688rem;}
+  .f28{font-size: 1.75rem;}
+  .f29{font-size: 1.813rem;}
+  .f30{font-size: 1.875rem;}
+  .f31{font-size: 1.938rem;}
+  .f32{font-size: 2rem;}
+  .f33{font-size: 2.063rem;}
+  .f34{font-size: 2.125rem;}
+  .f35{font-size: 2.188rem;}
+  .f36{font-size: 2.25rem;}
+  .f37{font-size: 2.313rem;}
+  .f38{font-size: 2.375rem;}
+  .f38{font-size: 2.438rem;}
+  .f40{font-size: 2.5rem;}
+
   .borda {
     border-color: #FF7272;
   }
