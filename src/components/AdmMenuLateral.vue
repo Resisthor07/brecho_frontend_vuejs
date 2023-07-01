7765b8c92b4fd4e15dd7eeeb430d63f5b8715d65
SamirCharleston <samircharleston@gmail.com>
2023-06-30 22:29:26 -0300

    salvando

--- src/components/AdmMenuLateral.vue
+++ src/components/AdmMenuLateral.vue
@@ -1,16 +1,115 @@
 <template>
-    <v-container class="cor-de-fundo h-tela">
-
+    <v-container class="cor-de-fundo h-tela pa-0 ma-0">
+        <div class="d-flex align-center justify-center h-10">
+            <h2 class="font-family font-title font-size font-style font-color">Brecho flor da fronteira</h2>
+        </div>
+        <v-divider color="white"></v-divider>
+        <div class="d-flex flex-column h-70">
+            <v-btn class="border-radius-0 cor-de-fundo font-color flat d-flex justify-start no-uppercase"
+                v-for="titulo in titulosDosBotoes" :key="titulo">{{
+                    titulo
+                }}</v-btn>
+        </div>
+        <v-divider color="white"></v-divider>
+        <div class="d-flex h-20">
+            <v-btn class="border-radius-0 cor-de-fundo font-color flat d-flex grow h-100">Sair</v-btn>
+        </div>
     </v-container>
 </template>
 <script lang="ts">
 export default {
     name: 'AdmMenuLateral',
-
+    data() {
+        return {
+            titulosDosBotoes: ['Inicio', 'Vendas', 'Pedidos', 'Configuracoes gerais'],
+        }
+    }
 }
 </script>
-<style scoped lang="scss">
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Dongle:wght@300;400;700&display=swap');
+
 .cor-de-fundo {
-    background-color: #FF7272;
+    background-color: #FF7272 !important;
+}
+
+.h-title {
+    height: 90px;
+}
+
+.h-10 {
+    height: 10%;
+}
+
+.h-20 {
+    height: 20%;
+}
+
+.h-70 {
+    height: 70%;
+}
+
+.h-100 {
+    height: 100%;
+}
+
+.font-color {
+    color: white !important;
+}
+
+.no-uppercase {
+    text-transform: none !important;
+}
+
+.flat {
+    box-shadow: none;
+}
+
+.justify-start {
+    justify-content: start;
+}
+
+.font-style {
+    font-family: 'Dongle', sans-serif;
+}
+
+.font-size {
+    font-size: 1.7em;
+    font-weight: 300;
+}
+
+.d-flex {
+    display: flex !important;
+}
+
+.flex-column {
+    flex-direction: column;
+}
+
+.align-center {
+    align-items: center;
+}
+
+.justify-space-between {
+    justify-content: space-between;
+}
+
+.borders-bt {
+    border-style: solid none;
+    border-width: 1px;
+    border-color: white;
+}
+
+.justify-center {
+    justify-content: center;
+}
+
+.border-radius-0 {
+    border-radius: 0;
+}
+
+.grow {
+    flex-grow: 1;
+    flex-basis: 0;
 }
 </style>
--- src/views/PaginaAdm.vue
+++ src/views/PaginaAdm.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <v-row>
-            <v-col cols="2">
+        <v-row class="pa-0 ma-0">
+            <v-col cols="2" class="pa-0 ma-0">
                 <AdmMenuLateral />
             </v-col>
-            <v-col>
+            <v-col class="pa-0 ma-0">
                 <div class="tabela"></div>
             </v-col>
         </v-row>
@@ -27,5 +27,67 @@ export default {
 <style lang="scss" scoped>
 .h-tela {
     height: 100vh;
+}
+
+.pa-0 {
+    padding: 0;
+}
+
+.ma-0 {
+    margin: 0;
+}
+
+$backgroud-primary-color: #FF7272;
+$button-togle-color: #f69a9a;
+$font-color: white;
+
+.h-70 {
+    height: 70%;
+}
+
+.button {
+    background-color: $backgroud-primary-color;
+    border: none;
+    height: 50px;
+    color: $font-color;
+    font-family: 'Dongle', sans-serif;
+    font-weight: lighter;
+    font-size: 1em;
+    text-transform: none;
+}
+
+.grow {
+    flex-grow: 1;
+    flex-basis: 0;
+}
+
+.button:hover {
+    background-color: $button-togle-color;
+}
+
+.bg-color-primary {
+    background-color: $backgroud-primary-color;
+}
+
+.borders-bt {
+    border-style: solid none;
+    border-width: 1px;
+    border-color: white;
+}
+
+.font-color {
+    color: white;
+}
+
+.font-big {
+    font-size: 1.5em;
+}
+
+.d-flex {
+    display: flex;
+}
+
+.flex-column {
+    flex-direction: column;
 }
 </style>
