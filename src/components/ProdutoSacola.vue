4a284dcb039c44a8e8e43937fbd54bb9032e9297
Thiago Cavalcante <116036410+Thiago-cavalcante-oliveira@users.noreply.github.com>
2023-07-03 14:35:26 -0300

    ajustando a página da sacola de compras

---
+++ src/components/ProdutoSacola.vue
@@ -0,0 +1,62 @@
+<template>
+<v-container>
+
+  <v-col cols="12" sm="6">
+  <v-row v-for="item in produto"
+  :key="item.id"
+  >
+    <v-col cols="12" sm="2">
+      <v-img
+          max-width="150"
+      :src="item.fotosDoProduto"
+      ></v-img>
+
+    </v-col>
+    <v-col cols="12" sm="4">
+      <v-row      >
+        {{item.nome}}
+      </v-row>
+      <v-row      >
+       Tam: {{item.tamanho}}
+      </v-row>
+
+    </v-col>
+    <v-col cols="12" sm="2">
+
+      {{formataValor(item.valorAtual)}}
+    </v-col>
+  </v-row>
+
+  </v-col>
+  <v-divider></v-divider>
+
+</v-container>
+</template>
+
+<script>
+export default {
+  name: "ProdutoSacola",
+
+  methods:{
+    formataValor(valor) {
+      return valor.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+    }
+
+  },
+
+  props:{
+    produto: Array
+
+  },
+
+  data:()=>({
+
+
+
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/sacolaCompras-View.vue
+++ src/views/sacolaCompras-View.vue
@@ -1,167 +1,27 @@
 <template>
 
   <main-layout>
-  
+   <v-container>
+     <v-row>
+       <v-col class="flex justify-center">
+         <v-icon size="40" color="#ff1617">mdi-shopping-outline</v-icon>
+         <span class="c313 dongle-regular f40 posicionamento_texto_desejo">Sacola de Compras</span>
+       </v-col>
+     </v-row>
+     <v-row>
+       <v-col class="flex justify-center">
+          <span class="c313 dongle-light f35" >{{
+              quantidade
+            }} items adicionado(s) em sua lista</span>
+       </v-col>
+     </v-row>
 
-    <v-conteiner>
-
-      <v-row class="organiza-header">
-        <v-col cols="12"
-        sm="12"
-        >
-            <div>                
-              <v-img src="../assets/sacolaCompras.png" height="30" contain></v-img>
-            </div>
-              <div class="titulo_sacola">
-                <span class="titulo_desejo">Sacola de Compras</span>
-                <span class="titulo_desejo" font-size="2px"> {{quantidade}} items adicionado(s) a sacola</span>
-              </div>
-        </v-col>
-      </v-row>
-
-      <v-divider :thickness="5" color="black" class="border-opacity-100"> </v-divider>
-
-      <v-row >
-        <v-col class="col_desejo d-flex justify-center ">
-          <span class="c313 dongle-light f35 " >{{
-              this.quantidade
-            }} items adicionado(s) Sacola</span>
-        </v-col>
-      </v-row>
-
-      <v-col  v-for="item in produtos" :key="item.id" >
-
-        <v-col cols="8"
-        sm="8">
-          <v-col cols="5"
-          sm="6"> 
-            <v-img src={{ item.fotosDoProduto}}></v-img>
-          </v-col>
-
-          <v-col cols="4"
-          sm="4">
-            <h1>nome: {{ item.nome }}</h1>
-            <span>Tamanho: {{ item.tamanho }}</span>
-            <span>Qnt: {{ item.quantidade }} </span>
-          </v-col>
-
-          <v-row cols="2" class="row-preco align-items-center">
-
-            <v-row>
-              <span class="font-weight-black ">Preço</span>
-            </v-row>
-            
-            <v-row>
-              <span class="font-weight-black"> R$ {{ item.valorAtual}}</span>
-            </v-row>
-
-          </v-row>
-
-          
-          
-        </v-col>
-
-        <v-divider :thickness="7" color="black" class="border-opacity-100" vertical></v-divider>
-
-        <v-col cols="4" class=" d-flex flex-column align-center">
-         
-       
-          <h1 class="mb-5"> Resumo da Compra</h1>
-          
-          <v-row justify="space-between" class="mt-5 ">
-            <v-col cols="12">
-              <div class="d-flex flex-column mt-5">
-                
-                <v-row cols="10" class="mb-2">
-                  <v-col >
-                    <span>Subtotal:</span>
-                  </v-col>
-                  <v-col >
-                    <span>R$ {{subTotal}}</span>
-                  </v-col>
-                </v-row>
-
-                <v-row cols="10" class="mb-2">
-                  <v-col>
-                    <span>Desconto:</span>
-                  </v-col>
-                  <v-col >
-                    <span>R$ -10,00</span>
-                  </v-col>
-                </v-row>
+    <produto-sacola :produto="produtos"></produto-sacola>
 
 
-      <v-divider color="black" class="border-opacity-100 mt-5"></v-divider>
 
-      <span class="mt-5 mb-10 font-weight-black">Valor Total: R$ 80,00</span>
-    </div>
-  </v-col>
-          </v-row>
-            <v-row class=" d-flex flex-column align-center">
-              <v-row cols="6">
-                <v-col 
-                  cols="12"
-                  
-                  class="text-center">
-                  <div  cols="6" class="text-center">
-                    <router-link :to="{name:'checkout-pagamento', params:{listaSacola: this.produtos} }">
-                      <v-btn
-                        rounded
-                        color="primary"
-                        dark
-                        sm="3"
-                        >
-
-                      Finalizar Compra
-                      </v-btn>
-                    </router-link>
-                  </div>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col 
-                  cols="12"
-                  
-                  class="text-center">
-                    <div cols="6"  class="text-center">
-                      <v-sheet>
-                      <router-link :to="{name:'home'}">
-                        <v-btn
-                        class="btn-compra borda-padrao w-100"
-                          rounded
-                          color="primary"
-                          dark
-                          sm="3"
-                          
-                        >
-                        <v-icon
-                          dark
-                          left
-                        >
-                          mdi-arrow-left
-                        </v-icon>
-                        Continuar Comprando
-                        </v-btn>
-                      </router-link>
-                    </v-sheet>
-                            
-                                
-                        
-                  </div>
-                </v-col>
-              </v-row>
-            
-
-            </v-row>
-          </v-col>
-
-        
-
-      </v-col>
-
-
-    </v-conteiner>
-        
+    </v-container>
+    <v-divider vertical></v-divider>
   </main-layout>
 
   
@@ -169,47 +29,67 @@
   
 <script>
 import MainLayout from '@/components/main-layout';
-import {useSacolaStore} from "@/store/sacolaStore";
-import {ProdutoClient} from "@/clients/produto-client"
+//import {useSacolaStore} from "@/store/sacolaStore";
+import ProdutoClient from "../clients/produto-client"
+import {useFavoritosStore} from "@/store/store";
+import ProdutoSacola from "@/components/ProdutoSacola";
 
 export default {
   name: "sacolaCompras",
   components: {
+    ProdutoSacola,
     MainLayout    
 },
   data: () => ({
+
+
+    ProdutoClient: new ProdutoClient(),
     produtos: [],
+
     vazio: true,
-    quantidade: 0,
-    produtoClient: new ProdutoClient(),
-    subTotal: 0,
+    quantidade:''
   }),
   created: function () {
-    console.log(this.quantidade)
+
     this.findById()
 
   },
   methods: {
 
-    async findById(){
+    async    findById(){
 
-      
-      const valor = useSacolaStore().listaSacola
+      const getApi = new ProdutoClient();
+      const valor = useFavoritosStore().listaFavoritos
       console.log(valor)
 
       valor.map((valor) => {
 
-        this.ProdutoClient.findById(valor).then((produto) => {
+        getApi.findById(valor).then((produto) => {
           this.produtos.push(produto)
-        this.quantidade=  this.produtos.length
-        this.subTotal += this.produto.valorAtual
-
+          this.quantidade=  this.produtos.length
 
         }).catch((error) =>{
           console.log(error);
         })
+      })},
 
-    })},
+    // async findById(){
+    //
+    //
+    //   const valor = useSacolaStore().listaSacola
+    //   console.log(valor)
+    //
+    //   valor.map((valor) => {
+    //
+    //     this.ProdutoClient.findById(valor).then((produto) => {
+    //       this.produtos.push(produto)
+    //     this.quantidade=  this.produtos.length
+    //
+    //     }).catch((error) =>{
+    //       console.log(error);
+    //     })
+    //
+    // })},
 
    
   },
