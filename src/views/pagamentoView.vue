46274012188036a8f448a71ad90a76cafa83e005
tailyne-bertoncelli <tailyne.berton@gmail.com>
2023-07-02 22:21:34 -0300

    Ajustando erros

--- src/views/pagamentoView.vue
+++ src/views/pagamentoView.vue
@@ -82,10 +82,9 @@ export default {
         adicionaPagamento() {
             VendaClient.cadastrar(this.venda)
                 .then(response => {
-                    this.venda = new VendaClient();
                     this.venda.pagamento = this.selectPagamento;
                     this.venda.produtos = this.listaProdutos;
-                    console.log(this.venda);
+                    console.log(response);  
                 }).catch (error => {
                     console.log(error);
                 })
